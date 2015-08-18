88d70ff30e627446a5b15b4f1e4c3e20998e7c1d
Sergio Milici <sergio.milici@gmail.com>
2015-08-18 23:23:52 +0000

    Creado archivo npm y eliminado dependencias.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
 var apuestasApp = angular.module('apuestasApp', ['ui.bootstrap']);
 
-apuestasApp.controller('apuestasCtrl', ['$scope', function($scope) {
-	$scope.apuesta = apuesta();
-	$scope.tiposApuesta = [ new pleno(), new docena() ];
-	$scope.open = function($event) {
-    	$event.preventDefault();
-    	$event.stopPropagation();
-	    $scope.opened = true;
-  	};
-  	$scope.minDateFechaApuesta = new Date();
-	$scope.apostar = function() {
-		try {
-			$scope.apuesta.apostar();
-		} catch (exception) {
-			console.log(exception);
-			var form = $scope.apuestasForm;
-			form.$invalid = true;
-			form.tipoApuesta.$error.message = exception;
-		}
-	};
+apuestasApp.controller('ApuestasController', [function () {
+  this.apuesta = apuesta();
+
+  this.tiposApuesta = [new pleno(), new docena()];
+
+  this.open = function ($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.opened = true;
+  };
+
+  this.minDateFechaApuesta = new Date();
+
+  this.apostar = function () {
+    try {
+      this.apuesta.apostar();
+    } catch (exception) {
+      console.log(exception);
+      var form = this.apuestasForm;
+      form.$invalid = true;
+      form.tipoApuesta.$error.message = exception;
+    }
+  };
+
 }]);
 
